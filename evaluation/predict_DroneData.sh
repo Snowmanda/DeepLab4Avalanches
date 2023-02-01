b2eb15dc433ef73d4739ab328df90902a53b0c16
Elyas Frei <73222583+Snowmanda@users.noreply.github.com>
2023-02-01 09:54:48 +0100

    last version

--- evaluation/predict_DroneData.sh
+++ evaluation/predict_DroneData.sh
@@ -4,5 +4,5 @@ python -m evaluation.predict_region_DroneData \
 --img_dir 'Drone_imagery_0.25' \
 --root_dir '/home/elyas/Desktop/SpotTheAvalanche/DroneData/Regions' \
 --dem_path '/home/elyas/Desktop/SpotTheAvalanche/DroneData/davos_dem_25cm_lv95.tif' \
---output_path '/home/elyas/Desktop/SpotTheAvalanche/DroneData/Output_Test' \
---checkpoint '/home/elyas/Desktop/SpotTheAvalanche/DroneData/Checkpoints/epoch=12-step=32850.ckpt' \
+--output_path '/home/elyas/Desktop/SpotTheAvalanche/DroneData/Output_AllPatches_Holdout' \
+--checkpoint '/home/elyas/Desktop/SpotTheAvalanche/DroneData/Checkpoints/epoch=17-step=33600.ckpt' \
